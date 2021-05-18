07b7649f9a4f4f3cee29da48fed79c256815cb8f
lina-ksouri <72580085+lina-ksouri@users.noreply.github.com>
2021-05-18 12:20:31 +0200

    Delete projet_jee directory

--- projet_jee/src/projet_jee/src/main/java/com/example/projet_jee/controller/chambreControl.java
+++
@@ -1,4 +0,0 @@
-package com.example.projet_jee.controller;
-
-public class chambreControl {
-}
--- projet_jee/src/projet_jee/src/main/java/com/example/projet_jee/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.projet_jee.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String msg){
-        super(msg);
-    }
-}
